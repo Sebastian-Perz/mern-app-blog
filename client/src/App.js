92c5f732a28d1b7d5fd850cd03480576a910295f
Sebastian-Perz <esdo.perz@gmail.com>
2021-01-10 20:23:30 +0100

    redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 
+import { getPosts } from "./actions/posts";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import postcards from "./images/postcards.png";
+
+import useStyles from "./styles";
 const App = () => {
+  const classes = useStyles();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
   return (
-    <div>
-      <h1>app</h1>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Postcards
+        </Typography>
+        <img
+          className={classes.image}
+          src={postcards}
+          alt="postcards"
+          height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing="3"
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Post = () => {
+  return <h1>single post</h1>;
+};
+
+export default Post;
